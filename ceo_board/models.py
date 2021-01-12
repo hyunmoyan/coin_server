fb483dc4c7b0bdc7e832b7e4a27ec1d4d312d463
hyunmoyan <pearlisrich@naver.com>
2021-01-12 09:39:19 +0900

    migration

--- ceo_board/models.py
+++ ceo_board/models.py
@@ -4,9 +4,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.CharField(max_length=20)
     title = models.CharField(max_length=144)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return '[{}] {}'.format(self.user.username, self.title)
+        return '[{}] {}'.format(self.user_id, self.title)
