34ed0c0b83e390d3c8407c8e3c76e694aeaa2587
hyunmoyan <pearlisrich@naver.com>
2021-01-12 10:30:14 +0900

    CRUD 구현
    post model을 만들어서 구현

--- ceo_board/models.py
+++ ceo_board/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_id = models.CharField(max_length=20)
+    user_id = models.CharField(max_length=20,default='')
     title = models.CharField(max_length=144)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- ceo_board/serializers.py
+++ ceo_board/serializers.py
@@ -2,17 +2,18 @@ from rest_framework import serializers
 from .models import Post
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email')
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'email')
 
 class PostSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    # user = UserSerializer(read_only=True)
     class Meta:
         model = Post
         fields = (
             'id',
+            'user_id',
             'title',
             'content',
             'created_at',
